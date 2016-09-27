d26823576aa080ae84301c036a9397c24e28737d
Kilbourne <luca.castellone@gmail.com>
2016-09-27 17:41:08 +0200

    Header & Footer

--- web/app/themes/pregit/templates/footer.php
+++ web/app/themes/pregit/templates/footer.php
@@ -1,5 +1,12 @@
 <footer class="content-info">
+<div class="logo"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/pregit_logo.svg" alt="" class="logo-img"></div>
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+          <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav']);
+      endif;
+      ?>
   </div>
+  <div class="last-line"><span class="left"> © 2016 I Pregi d’Italia </span><span class="right">Credits</span>
+   </div>
 </footer>
