6d3935a8d5203913d5e547d35e38d86a6d450623
Kilbourne <luca.castellone@gmail.com>
2016-10-02 19:13:18 +0200

    Produttore form @

--- web/app/themes/pregit/functions.php
+++ web/app/themes/pregit/functions.php
@@ -26,3 +26,24 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+
+add_filter( 'gform_column_input_1_5_3', 'set_column', 10, 5 );
+function set_column( $input_info, $field, $column, $value, $form_id ) {
+    return array( 'type' => 'date');
+}
+add_filter( 'gform_column_input_1_5_1', 'set_column2', 10, 5 );
+function set_column2( $input_info, $field, $column, $value, $form_id ) {
+    return array( 'type' => 'post-select','args'=>array(
+      'post_type' => 'product',
+      'meta_query' => array(
+    array(
+      'key' => 'produttore',
+      'value' => '3',
+      'compare' => 'LIKE'
+    ))
+      ),'value'=>'ID',"text"=>'post_title' );
+}
+
+// Disabilità opzione se già usate
+// se max row
