90be77324317e089fc77ee509c5b8ae5039ae78f
Kilbourne <luca.castellone@gmail.com>
2016-10-03 17:48:56 +0200

    Extras: Product Form hidden id

--- web/app/themes/pregit/lib/extras.php
+++ web/app/themes/pregit/lib/extras.php
@@ -31,3 +31,13 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+add_filter('single_product_large_thumbnail_size', __NAMESPACE__ . '\\pregit_single_product_thumb_size' );
+function pregit_single_product_thumb_size(){
+  return 'full';
+}
+add_filter( 'gform_field_value_product_id', __NAMESPACE__ . '\\my_custom_population_function' );
+function my_custom_population_function( $value ) {
+ global $post; 
+ return $post->ID;
+}
