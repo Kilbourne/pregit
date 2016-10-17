2b26f5f578948c333dbc01352fbffd08627ba111
Kilbourne <luca.castellone@gmail.com>
2016-10-17 17:43:00 +0200

    Template

---
+++ web/app/themes/pregit/archive-taxonomy-producer.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * Template Name: Archive Taxonomy Producer
+ */
+?>
+
+<div class="producers-wrapper">
+<?php 	
+	$args=[
+		 'taxonomy'=>'producer'		 
+	];
+	$terms=get_terms($args);
+	if($terms){
+?>
+
+ 	<ul class="producers-list">	
+<?php
+	foreach ($terms as $key => $term) {
+		$term_id=$term->term_id;
+?>
+<li class="term-producer">	
+<a href="<?php echo esc_url( get_term_link( $term_id ) ); ?>">		
+			<?php echo wp_get_attachment_image(get_field('logo', 'producer_'.$term_id))?wp_get_attachment_image(get_field('logo', 'producer_'.$term_id)):''; ?>
+		</a>
+</li> 
+<?php
+	}
+?>
+   	</ul>
+<?php 	} ?>
+</div> 
