26a6ca1a88939a8d9ad480f79d6ab227b4af49d1
Kilbourne <luca.castellone@gmail.com>
2017-02-13 02:00:57 +0100

    hghfh

--- public_html/app/themes/pregit/archive-taxonomy-producer.php
+++ public_html/app/themes/pregit/archive-taxonomy-producer.php
@@ -1,31 +1,32 @@
-<?php 
+<?php
 /**
  * Template Name: Archive Taxonomy Producer
  */
 ?>
 
 <div class="producers-wrapper">
-<?php 	
+<?php
 	$args=[
-		 'taxonomy'=>'producer'		 
+		 'taxonomy'=>'producer'
 	];
 	$terms=get_terms($args);
 	if($terms){
 ?>
 
- 	<ul class="producers-list">	
+ 	<ul class="producers-list">
 <?php
 	foreach ($terms as $key => $term) {
 		$term_id=$term->term_id;
 ?>
-<li class="term-producer">	
-<a href="<?php echo esc_url( get_term_link( $term_id ) ); ?>">		
+<li class="term-producer">
+<h2 hidden><?php the_title(); ?></h2>
+<a href="<?php echo esc_url( get_term_link( $term_id ) ); ?>">
 			<?php echo wp_get_attachment_image(get_field('logo', 'producer_'.$term_id))?wp_get_attachment_image(get_field('logo', 'producer_'.$term_id)):''; ?>
 		</a>
-</li> 
+</li>
 <?php
 	}
 ?>
    	</ul>
 <?php 	} ?>
-</div> 
+</div>
