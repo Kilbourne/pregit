90dc8d50142bb4077d33e525b572a470db4c1f37
Kilbourne <luca.castellone@gmail.com>
2017-03-01 16:13:11 +0100

    update

--- public_html/app/themes/pregit/templates/head.php
+++ public_html/app/themes/pregit/templates/head.php
@@ -12,5 +12,4 @@ remove_action( 'wp_head',  array( $tracking, 'ga_tracking_code' ), 9 );
       add_action( 'body_open',  function() use ($tracking) { Extras\ga_tracking_code($tracking);}, 1 );
       }
       wp_head(); ?>
-      <meta name="google-site-verification" content="qW7Iwcut0nCnDeqjX91qUshR8OOGR6hhcwaXQnwd5u4" />
 </head>
